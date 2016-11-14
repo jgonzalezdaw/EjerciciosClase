30541cb781081ebe23183abeb97154cea5c598bf
Carlos Sogorb <profesorderedes@gmail.com>
2016-11-14 19:08:11 +0100

    lun nov 14 19:08:11 CET 2016

---
+++ Tema3/src/ejemplos/Ejemplo1.java
@@ -0,0 +1,14 @@
+package ejemplos;
+
+public class Ejemplo1 {
+	public static void main(String[] args) {
+
+		double[] precios = { 102, 20.50, 30, 67, 12, 10 };
+
+		for (int i = 0; i < precios.length; i++) {
+			System.out.println("Posición " + i + ": " + precios[i]);
+		}
+		
+
+	}
+}
---
+++ Tema3/src/ejemplos/Ejemplo2.java
@@ -0,0 +1,21 @@
+package ejemplos;
+
+public class Ejemplo2 {
+
+	public static void main(String[] args) {
+		
+		String[] nombres = new String[4];
+		
+		nombres[0] = "Carlos";
+		nombres[1] = "Teo";
+		nombres[2] = "Gustavo";
+		nombres[3] = "Patryck";
+		
+		for(int i=0; i<nombres.length;i++){
+			System.out.println(nombres[i]);
+		}
+		
+		
+	}
+	
+}
