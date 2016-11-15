69f2bf8cdfa6ad985f620e91c4e674c09f6e85a6
Carlos Sogorb <profesorderedes@gmail.com>
2016-11-15 19:15:31 +0100

    mar nov 15 19:15:31 CET 2016

--- Tema3/src/ejemplos/Ejemplo4.java
+++ Tema3/src/ejemplos/Ejemplo4.java
@@ -1,5 +1,38 @@
 package ejemplos;
 
+import java.util.*;
+
 public class Ejemplo4 {
 
+	public static void main(String[] args) {
+
+		// Leemos cuatro precios desde la terminal y los guardamos en
+		// un array.
+
+		Scanner entrada = new Scanner(System.in);
+		double[] precios = new double[4];
+		double precio;
+
+		for (int i = 0; i < precios.length; i++) {
+
+			System.out.print("Precio " + i + ": ");
+			precio = entrada.nextDouble();
+
+			precios[i] = precio;
+
+		}
+
+		System.out.println();
+		
+		for (int i = 0; i < precios.length; i++) {
+			System.out.println("Precio " + i + ": " + precios[i]);
+		}
+
+		System.out.println("Hecho.");
+
+	}
 }
+
+
+
+
