a7c5cce0c601add5cfbc64ba83193b85683594da
Carlos Sogorb <profesorderedes@gmail.com>
2016-11-17 16:35:11 +0100

    jue nov 17 16:35:11 CET 2016

--- Tema3/src/ejemplos/Ejemplo10.java
+++ Tema3/src/ejemplos/Ejemplo10.java
@@ -2,14 +2,12 @@ package ejemplos;
 
 import java.util.*;
 
-public class Ejemplo9 {
+public class Ejemplo10 {
 
 	public static void main(String[] args) {
 
-		// Arrays incompletos
-		// Crea un array de enteros de 1000 elementos y ve pidiendo
-		// edades de alumnos. Cuando el usuario escriba -1, el programa
-		// terminará y mostrará todas las edades introducidas.
+		// Modificar el ejemplo 9 de manera que
+		// ambos bucles sean de tipo FOR
 
 		Scanner entrada = new Scanner(System.in);
 		int[] edades = new int[1000];
@@ -17,16 +15,18 @@ public class Ejemplo9 {
 		// Leemos los datos.
 		System.out.println("Ve introduciendo las edades " + "de los alumnos y -1 cuando hayas acabado");
 		int edad;
-		int i = 0;
-		do {
+
+		for (int i = 0; i < edades.length; i++) {
 			System.out.print("Edad: ");
 			edad = entrada.nextInt();
 			edades[i] = edad;
-			i++;
-		} while (edad != -1 && i < edades.length);
+			if (edad == -1) {
+				break;
+			}
+		}
 
 		// Mostramos los datos del array.
-		for (i = 0; i < edades.length; i++) {
+		for (int i = 0; i < edades.length; i++) {
 			if (edades[i] != -1) {
 				System.out.println(edades[i]);
 			} else {
@@ -37,8 +37,3 @@ public class Ejemplo9 {
 	}
 
 }
-
-
-
-
-
--- Tema3/src/ejemplos/Ejemplo11.java
+++ Tema3/src/ejemplos/Ejemplo11.java
@@ -2,17 +2,15 @@ package ejemplos;
 
 import java.util.*;
 
-public class Ejemplo9 {
+public class Ejemplo11 {
 
 	public static void main(String[] args) {
 
-		// Arrays incompletos
-		// Crea un array de enteros de 1000 elementos y ve pidiendo
-		// edades de alumnos. Cuando el usuario escriba -1, el programa
-		// terminará y mostrará todas las edades introducidas.
+		// Modificar el ejemplo 9 de manera que
+		// ambos bucles sean de tipo while
 
 		Scanner entrada = new Scanner(System.in);
-		int[] edades = new int[1000];
+		int[] edades = new int[5];
 
 		// Leemos los datos.
 		System.out.println("Ve introduciendo las edades " + "de los alumnos y -1 cuando hayas acabado");
@@ -26,19 +24,15 @@ public class Ejemplo9 {
 		} while (edad != -1 && i < edades.length);
 
 		// Mostramos los datos del array.
-		for (i = 0; i < edades.length; i++) {
-			if (edades[i] != -1) {
-				System.out.println(edades[i]);
-			} else {
+		i = 0;
+		while (i < edades.length) {
+			if (edades[i] == -1) {
 				break;
 			}
+			System.out.println(edades[i]);
+			i++;
 		}
 
 	}
 
 }
-
-
-
-
-
