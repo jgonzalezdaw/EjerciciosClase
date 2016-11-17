5a1757fb9f6e94ff24a7e6d3572c7fb51b5fd64e
Carlos Sogorb <profesorderedes@gmail.com>
2016-11-17 15:44:40 +0100

    jue nov 17 15:44:40 CET 2016

---
+++ Tema3/src/ejemplos/Ejemplo9.java
@@ -0,0 +1,40 @@
+package ejemplos;
+
+import java.util.*;
+
+public class Ejemplo9 {
+
+	public static void main(String[] args) {
+
+		// Arrays incompletos
+		// Crea un array de enteros de 1000 elementos y ve pidiendo
+		// edades de alumnos. Cuando el usuario escriba -1, el programa
+		// terminará y mostrará todas las edades introducidas.
+
+		Scanner entrada = new Scanner(System.in);
+		int[] edades = new int[1000];
+
+		// Leemos los datos.
+		System.out.println("Ve introduciendo las edades " + 
+				"de los alumnos y -1 cuando hayas acabado");
+		int edad;
+		int i = 0;
+		do {
+			System.out.print("Edad: ");
+			edad = entrada.nextInt();
+			edades[i] = edad;
+			i++;
+		} while (edad != -1);
+
+		// Mostramos los datos del array.
+
+	}
+
+}
+
+
+
+
+
+
+
