7c6182460ec6fd74060d0edc31d626ca643bc634
Carlos Sogorb <profesorderedes@gmail.com>
2016-11-30 19:38:45 +0100

    mié nov 30 19:38:44 CET 2016

---
+++ Tema3/src/ejemplos/Ejemplo13.java
@@ -0,0 +1,32 @@
+package ejemplos;
+
+/**
+ * Ejemplo de obtención del mayor elemento de un array de valores,
+ * guardando en una variable el mayor valor.
+ * @author csogorb
+ *
+ */
+public class Ejemplo13 {
+	public static void main(String[] args) {
+
+		int[] temperaturas = { 5, 1, 3, 10, 8 };
+
+		// Suponemos que el mínimo es el primer dato del array.
+		int max = temperaturas[0];
+
+		// Recorremos el array comparando cada posición con el valor
+		// guardado en min.
+		for (int i = 1; i < temperaturas.length; i++) {
+
+			// Si encontramos un valor más pequeño que min,
+			// lo intercambiamos.
+			if (temperaturas[i] > max) {
+				max = temperaturas[i];
+			}
+
+		}
+
+		System.out.println("La mayor temperatura es " + max + "ºC.");
+
+	}
+}
---
+++ Tema3/src/ejemplos/Ejemplo14.java
@@ -0,0 +1,41 @@
+package ejemplos;
+
+/**
+ * Ejemplo de obtención del menor elemento de un array de valores,
+ * guardando en una variable la posición del menor valor.
+ * @author csogorb
+ *
+ */
+public class Ejemplo14 {
+
+	public static void main(String[] args) {
+		
+		int[] temperaturas = { 5, 1, 3, 10, 8 };
+		
+		// Suponemos que el mínimo es el primer dato del array.
+		int posMin = 0;
+		
+		// Recorremos el array comparando cada posición con el valor
+		// guardado en la posición posMin.
+		for (int i = 0; i < temperaturas.length; i++) {
+			
+			// Si encontramos un valor más pequeño que el de posMin,
+			// lo intercambiamos.
+			
+			
+		}
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
