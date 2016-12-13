a47accc922cb2bfc216a46f7ba1b84f93fb64bcf
Carlos Sogorb <profesorderedes@gmail.com>
2016-12-13 19:15:41 +0100

    mar dic 13 19:15:41 CET 2016

--- Tema3/src/hoja2ejercicios/Ejercicio15.java
+++ Tema3/src/hoja2ejercicios/Ejercicio15.java
@@ -10,25 +10,38 @@ public class Ejercicio15 {
 		System.out.println("Artículos");
 		System.out.println("=======================");
 		for (int i = 0; i < precios.length; i++) {
-			System.out.println(precios[i][0] + "\t" 
-					+ precios[i][1]);
+			System.out.println(precios[i][0] + "\t" + precios[i][1]);
 		}
-		
+
 		float total = 0;
-		for(int i=0; i < precios.length; i++){
+		for (int i = 0; i < precios.length; i++) {
 			total = total + Float.parseFloat(precios[i][1]);
 		}
-		
+
 		System.out.println("\nTotal: " + total + " €.");
+
+		int posMin = 0;
+		int posMax = 0;
+		float precio;
+		float min;
+		float max;
+		for (int i = 0; i < precios.length; i++) {
+			precio = Float.parseFloat(precios[i][1]);
+			min = Float.parseFloat(precios[posMin][1]);
+			max = Float.parseFloat(precios[posMax][1]);
+			
+			if (precio < min) {
+				posMin = i;
+			}
+
+			if (precio > max) {
+				posMax = i;
+			}
+		}
+		
+		System.out.println("Precio mínimo: " + precios[posMin][1]);
+		System.out.println("Precio máximo: " + precios[posMax][1]);
 
 	}
 
 }
-
-
-
-
-
-
-
-
