7475ca62f5e38ff35ac3e8ead9bbfbbbde6428bd
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-09 18:42:47 +0100

    lun ene  9 18:42:47 CET 2017

---
+++ Tema4/src/EjemplosMetodos.java
@@ -0,0 +1,35 @@
+
+public class EjemplosMetodos {
+
+	public static void main(String[] args) {
+
+		System.out.println("Llamando al mayordomo...");
+		mayordomo();
+		System.out.println("Más instrucciones...");
+
+		System.out.println("\nÉsta es la tabla del 5:");
+		tablaDel5();
+
+	}
+
+	// Muestra una frase en pantalla.
+	static void mayordomo() {
+		System.out.println("¿Qué se le ofrece, señor?");
+	}
+
+	// Método que escribe la tabla de multiplicar del 5.
+	static void tablaDel5() {
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("5 x " + i + " = " + 5 * i);
+		}
+
+	}
+
+}
+
+
+
+
+
+
