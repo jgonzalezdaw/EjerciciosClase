8ac5ffcb5ec4e3665e7315d5114f7fdecc2c0763
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-09 18:20:29 +0100

    lun ene  9 18:20:28 CET 2017

---
+++ Tema3/src/cadenas/Cadenas.java
@@ -0,0 +1,31 @@
+package cadenas;
+
+public class Cadenas {
+
+	public static void main(String[] args) {
+
+		String nombre = "Carlos";
+		
+		System.out.println("Longitud de la cadena de caracteres: " 
+				+ nombre.length());
+		
+		System.out.println("La tercera letra del nombre es " 
+				+ nombre.charAt(2));
+		
+		System.out.println("Si cambio las 's' por 't' el nombre queda: " 
+				+ nombre.replace('s', 't'));
+		
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
