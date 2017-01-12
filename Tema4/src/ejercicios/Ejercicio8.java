457b3c4f280758fa41d9657898675f2e8f884d57
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-12 16:17:01 +0100

    jue ene 12 16:17:01 CET 2017

---
+++ Tema4/src/ejercicios/Ejercicio8.java
@@ -0,0 +1,41 @@
+package ejercicios;
+
+public class Ejercicio8 {
+
+	public static void main(String[] args) {
+		System.out.println("El menor de 3 y 10 es " + min(3, 10));
+		System.out.println("El mayor de 3 y 10 es " + max(3, 10));
+	}
+
+	static int max(int x, int y) {
+		if (x > y) {
+			return x;
+		} else {
+			return y;
+		}
+	}
+
+	static int min(int x, int y) {
+		return x < y ? x : y;	// Usando el operador ternario para abreviar.
+		//
+		// if(x < y){
+		// return x;
+		// }else{
+		// return y;
+		// }
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
