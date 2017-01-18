1993db76d31aea2e4a68426d5af9d5ce44d9f955
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-18 19:04:09 +0100

    mié ene 18 19:04:08 CET 2017

---
+++ Tema5/src/ejemplo3/Circulo.java
@@ -0,0 +1,17 @@
+package ejemplo3;
+
+public class Circulo {
+
+	// Atributos
+	int x, y;	// Coordenadas del centro.
+	String color;
+	int radio;
+	
+	// Métodos
+	void mostrar(){
+		System.out.println("Coordenadas: (" + x + "," + y + ")");
+		System.out.println("Radio: " + radio);
+		System.out.println("Color: " + color);
+	}
+	
+}
---
+++ Tema5/src/ejemplo3/Ejemplo3.java
@@ -0,0 +1,14 @@
+package ejemplo3;
+
+public class Ejemplo3 {
+	public static void main(String[] args) {
+		Circulo c1 = new Circulo();
+		
+		c1.x = 10;
+		c1.y = 10;
+		c1.radio = 5;
+		c1.color = "rojo";
+		
+		
+	}
+}
