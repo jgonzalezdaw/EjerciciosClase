33fce39ad5c2436420e181a3074bb94be7c8f0b2
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-19 16:40:21 +0100

    jue ene 19 16:40:21 CET 2017

---
+++ Tema5/src/ejemplo10/Ejemplo10.java
@@ -0,0 +1,34 @@
+package ejemplo10;
+
+/**
+ * En este ejemplo hacemos que un constructor llame a otro.
+ * 
+ * @author csogorb
+ *
+ */
+public class Ejemplo10 {
+	public static void main(String[] args) {
+		Circulo c1 = new Circulo();
+		c1.mostrar();
+		
+		System.out.println();
+		
+		Circulo c2 = new Circulo(10,20,"amarillo",50);
+		c2.mostrar();
+		
+		System.out.println();
+		
+		Circulo c3 = new Circulo(15,33);
+		c3.mostrar();
+
+	}
+}
+
+
+
+
+
+
+
+
+
