22cb1a5d9f6081020a7782f318c0ce8da1ebd1d9
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-19 16:29:52 +0100

    jue ene 19 16:29:52 CET 2017

---
+++ Tema5/src/ejemplo8/Ejemplo8.java
@@ -0,0 +1,42 @@
+package ejemplo8;
+
+/**
+ * En este ejemplo hacemos que un constructor llame a otro.
+ * 
+ * @author csogorb
+ *
+ */
+public class Ejemplo8 {
+
+	public static void main(String[] args) {
+
+		Persona p1;
+		p1 = new Persona("82911922H", "Jose Antonio", "Coca", 21);
+
+		System.out.println("Nombre completo: " + p1.nombre + " " + p1.apellidos);
+
+		if (p1.edad >= 18) {
+			System.out.println("Puede entrar a la discoteca.");
+		} else {
+			System.out.println("No puede entrar a la discoteca.");
+		}
+
+		Persona p2 = new Persona();
+		p2.dni = "3782363H";
+		p2.nombre = "Gustavo";
+		p2.apellidos = "Baquerizo";
+		p2.edad = 21;
+
+		System.out.println("Me llamo " + p2.nombre + " " + p2.apellidos + " y tengo " + p2.edad + " años.");
+
+		// Utilizamos el método mostrar()
+		System.out.println("\nDatos de p1:\n");
+		p1.mostrar();
+
+		System.out.println("Creamos una tercera persona");
+		Persona p3 = new Persona("Carlos", "Sogorb");
+		p3.mostrar();
+
+	}
+
+}
