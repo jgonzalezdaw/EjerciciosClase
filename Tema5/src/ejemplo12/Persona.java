4714d5d99806dff64d33ef5454dcd284a0261743
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-23 19:07:16 +0100

    lun ene 23 19:07:16 CET 2017

---
+++ Tema5/src/ejemplo12/Persona.java
@@ -0,0 +1,44 @@
+package ejemplo12;
+
+public class Persona {
+
+	// Atributos
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private int edad;
+
+	// Constructores
+	Persona(String dni, String nombre, String apellidos, int edad) {
+		System.out.println("Se ha llamado al constructor " + "Persona(dni, nombre, apellidos, edad)");
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+
+	}
+
+	Persona() {
+		System.out.println("Se ha llamado al constructor Persona()");
+	}
+
+	Persona(String nombre, String apellidos) {
+
+		this("00000000A", nombre, apellidos, 30);
+
+		// this.nombre = nombre;
+		// this.apellidos = apellidos;
+		// Damos a dni y edad unos valores por defecto.
+		// dni = "0000000A";
+		// edad = 30;
+	}
+
+	// Métodos
+	void mostrar() {
+		System.out.println("DNI: " + dni);
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Apellidos: " + apellidos);
+		System.out.println("Edad: " + edad);
+	}
+
+}
