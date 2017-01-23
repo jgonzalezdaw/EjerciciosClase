fd7d8ee497b4393238e184a4a564b73cf48db038
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-23 18:48:56 +0100

    lun ene 23 18:48:56 CET 2017

---
+++ Tema4/src/ejercicios/Ejercicio12.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class Ejercicio12 {
+
+	public static void main(String[] args) {
+		// 12.- Desarrolla modularmente (usando tres métodos,
+		// además de main( )) un programa que solicite
+		// por consola una lista de números enteros,
+		// los ordene y los muestre en pantalla.
+
+		// llamamos al metodo introducir sin pasarle ningun valor
+		int[] valores = introducir();
+		ordenar(valores);
+		imprimir(valores);
+	}
+
+	static int[] introducir() {
+		Scanner input = new Scanner(System.in);
+		// introduciremos los numeros en un array
+		int numeroStr[] = new int[4];
+		for (int i = 0; i < numeroStr.length; i++) {
+			System.out.println("Introduce un numero:");
+			numeroStr[i] = input.nextInt();
+		}
+		// llamamos al metodo ordenar pasando el array
+		return numeroStr;
+	}
+
+	static void ordenar(int[] numeroStr) {
+		int posMin = 0;
+		int aux;
+		
+		// ordenamos el array
+		for (int i = 0; i < numeroStr.length - 1; i++) {
+			posMin = i;
+			for (int j = i + 1; j < numeroStr.length; j++) {
+				if (numeroStr[posMin] > numeroStr[j]) {
+					posMin = j;
+				}
+			}
+			aux = numeroStr[i];
+			numeroStr[i] = numeroStr[posMin];
+			numeroStr[posMin] = aux;
+		}
+	}
+
+	static void imprimir(int[] numeros) {
+		// en el metodo imprimir mostramos
+		// los numeros ordenados
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println(numeros[i]);
+		}
+	}
+}
--- Tema4/src/ejercicios/Prueba.java
+++ Tema4/src/ejercicios/Prueba.java
@@ -1,5 +1,37 @@
 package ejercicios;
 
-public class Prueba {
+import java.util.Scanner;
+
+public class Prueba { // Ejercicio con un solo return cuando se piden dos.
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Introduzca el nombre del planeta: ");
+		String planeta = entrada.nextLine();
+		System.out.println(planetas(planeta));
+
+	}
+
+	static String planetas(String planet) {
+
+		String planetas[] = { "Mercurio", "Venus", "Tierra", "Marte", "Jupiter", "Saturno", "Uranio", "Neptuno",
+				"Plutón" };
+
+		String planets = null;
+
+		for (int i = 0; i < planetas.length; i++) {
+
+			if (planet.equals(planetas[i])) {
+				planets = String.valueOf(i);
+				break;
+
+			} else {
+				planets = "-1";
+			}
+		}
+		return planets;
+
+	}
 
 }
