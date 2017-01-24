261d436a357f7d8f78b66c8d1ea43e5734690292
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-24 19:10:20 +0100

    mar ene 24 19:10:20 CET 2017

---
+++ Tema5/src/ejemplo15/Ejemplo15.java
@@ -0,0 +1,35 @@
+package ejemplo15;
+
+/**
+ * En este ejemplo, agregamos constructores a la clase Rectangulo
+ * 
+ * @author csogorb
+ * 
+ */
+public class Ejemplo15 {
+
+	public static void main(String[] args) {
+		Rectangulo rect1 = new Rectangulo();
+		rect1.mostrar();
+		
+		System.out.println();
+		
+		Rectangulo rect2 = new Rectangulo(5,5,20,10,"verde");
+		rect2.mostrar();
+		
+		System.out.println();
+		
+		Rectangulo rect3 = new Rectangulo("verde");
+		rect3.mostrar();
+	}
+
+}
+
+
+
+
+
+
+
+
+
---
+++ Tema5/src/ejemplo15/Rectangulo.java
@@ -0,0 +1,112 @@
+package ejemplo15;
+
+public class Rectangulo {
+
+	// Atributos
+	private int x, y;
+	private int base, altura;
+	private String color;
+
+	// Constructores
+	Rectangulo() {
+
+	}
+
+	Rectangulo(int x, int y, int base, int altura, 
+			String color) {
+		this.x = x;
+		this.y = y;
+		this.base = base;
+		this.altura = altura;
+		this.color = color;
+	}
+	
+	Rectangulo(String color){
+		this(0,0,0,0,color);
+	}
+
+	// Métodos
+	void mostrar() {
+		System.out.println("Coordenadas: (" + x + "," + y + ")");
+		System.out.println("Tamaño: " + base + "x" + altura);
+		System.out.println("Color: " + color);
+	}
+
+	int area() {
+		return base * altura;
+	}
+
+	int perimetro() {
+		return 2 * base + 2 * altura;
+	}
+
+	void mover(int dx, int dy) {
+		x = x + dx;
+		y = y + dy;
+	}
+
+	boolean estaDentro(int a, int b) {
+		if (a > x && a < x + base && b > y - altura && b < y) {
+			return true;
+		}
+
+		return false;
+	}
+	
+	// Métodos de acceso
+	public int getX(){
+		return x;
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getBase() {
+		return base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
