c9dc789db7bdd8ad0b6fe345f49614b86840df70
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-24 18:37:47 +0100

    mar ene 24 18:37:47 CET 2017

--- Tema5/src/ejemplo12/Persona.java
+++ Tema5/src/ejemplo12/Persona.java
@@ -41,4 +41,42 @@ public class Persona {
 		System.out.println("Edad: " + edad);
 	}
 
+	// Métodos de acceso.
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
 }
+
+
+
+
+
