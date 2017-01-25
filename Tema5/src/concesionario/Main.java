646bda9b318a43872c7474ddac4a8178e855fc0c
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-25 19:00:48 +0100

    mié ene 25 19:00:48 CET 2017

---
+++ Tema5/src/concesionario/Main.java
@@ -0,0 +1,18 @@
+package concesionario;
+
+//Ejercicio
+//
+//1. Desarrollar una clase Coche (y una clase Main para utilizarla).
+//1. Atributos: marca, modelo, color, matricula, precio y descuento.
+//2. Métodos: arrancar, detenerse, acelerar, frenar, verPrecio 
+//(devuelve un valor a partir del precio y el descuento).
+//2. Desde la clase Main crear un par de objetos de la clase Coche. 
+//Asignar un valor a la variable descuento de uno de ellos y comprobar 
+//que ese valor se ha cambiado en todos los demás.
+//3. Comprobar que se puede acceder a la variable descuento usando 
+//el nombre de la clase en lugar del nombre de una instancia: Coche.descuento.
+
+
+public class Main {
+
+}
