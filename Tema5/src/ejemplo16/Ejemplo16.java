004f76ca4d2c043b54a7bef256ff54930ec51407
Carlos Sogorb <profesorderedes@gmail.com>
2017-01-25 18:57:22 +0100

    mié ene 25 18:57:22 CET 2017

---
+++ Tema5/src/ejemplo16/Ejemplo16.java
@@ -0,0 +1,47 @@
+package ejemplo16;
+
+/**
+ * En este ejemplo hacemos que un constructor llame a otro.
+ * 
+ * @author csogorb
+ *
+ */
+public class Ejemplo16 {
+
+	public static void main(String[] args) {
+
+		Persona p1;
+		p1 = new Persona("82911922H", "Jose Antonio", "Coca", 21);
+		System.out.println("\n***Hay " + Persona.contadorPersonas + " personas.");
+
+		System.out.println("Nombre completo: " + p1.getNombre() + " " + p1.getApellidos());
+
+		if (p1.getEdad() >= 18) {
+			System.out.println("Puede entrar a la discoteca.");
+		} else {
+			System.out.println("No puede entrar a la discoteca.");
+		}
+
+		Persona p2 = new Persona();
+		System.out.println("\n***Hay " + Persona.contadorPersonas + " personas.");
+		p2.setDni("3782363H");
+		// p2.nombre = "Gustavo";
+		p2.setNombre("Gustavo");
+		p2.setApellidos("Baquerizo");
+		p2.setEdad(21);
+
+		System.out.println(
+				"Me llamo " + p2.getNombre() + " " + p2.getApellidos() + " y tengo " + p2.getEdad() + " años.");
+
+		// Utilizamos el método mostrar()
+		System.out.println("\nDatos de p1:\n");
+		p1.mostrar();
+
+		System.out.println("Creamos una tercera persona");
+		Persona p3 = new Persona("Carlos", "Sogorb");
+		System.out.println("\n***Hay " + Persona.contadorPersonas + " personas.");
+		p3.mostrar();
+
+	}
+
+}
---
+++ Tema5/src/ejemplo16/Persona.java
@@ -0,0 +1,81 @@
+package ejemplo16;
+
+public class Persona {
+
+	// Atributos
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private int edad;
+	
+	// Atributo que contará cuántos objetos de la clase  
+	// Persona se han creado.
+	static int contadorPersonas; 
+
+	// Constructores
+	Persona(String dni, String nombre, String apellidos, int edad) {
+		System.out.println("Se ha llamado al constructor " + "Persona(dni, nombre, apellidos, edad)");
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+		
+		contadorPersonas++;
+	}
+
+	Persona() {
+		System.out.println("Se ha llamado al constructor Persona()");
+		contadorPersonas++;
+	}
+
+	Persona(String nombre, String apellidos) {
+		this("00000000A", nombre, apellidos, 30);
+	}
+
+	// Métodos
+	void mostrar() {
+		System.out.println("DNI: " + dni);
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Apellidos: " + apellidos);
+		System.out.println("Edad: " + edad);
+	}
+
+	// Métodos de acceso.
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+}
+
+
+
+
+
