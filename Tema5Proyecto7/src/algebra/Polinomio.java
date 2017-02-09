b11beb1421b79a817e60d1a85992d411a5e32715
Carlos Sogorb <profesorderedes@gmail.com>
2017-02-09 15:54:59 +0100

    jue feb  9 15:54:59 CET 2017

--- Tema5Proyecto7/src/algebra/Polinomio.java
+++ Tema5Proyecto7/src/algebra/Polinomio.java
@@ -4,58 +4,96 @@ public class Polinomio {
 
 	// Atributos
 	private double coeficientes[];
-	
+
 	// Constructores
-	public Polinomio(){
+	public Polinomio() {
 		// Creamos un polinomio de grado 2 con
 		// coeficientes 0.
 		coeficientes = new double[3];
 	}
-	
-	public Polinomio(int grado){
-		// Creamos un polinomio del grado indicado 
+
+	public Polinomio(int grado) {
+		// Creamos un polinomio del grado indicado
 		// con coeficientes 0.
-		coeficientes = new double[grado+1];
+		coeficientes = new double[grado + 1];
 	}
-	
-	public Polinomio(double[] coefs){
+
+	public Polinomio(double[] coefs) {
 		coeficientes = coefs;
 	}
-	
-	public int getGrado(){
+
+	public int getGrado() {
 		return coeficientes.length - 1;
 	}
 
-	// Suma un polinomio p a nuestro polinomio.
+	// Suma un polinomio q a nuestro polinomio.
 	// Damos por supuesto que ambos son del mismo grado.
-	public Polinomio sumar(Polinomio q){
-		
+	public Polinomio sumar(Polinomio q) {
+
 		// Obtenemos los coeficientes de q
 		double[] coeficientesQ = q.getCoeficientes();
-		
+
 		// Creamos un array de coeficientes que contendrá
 		// la suma de los coeficientes de los dos polinomios.
 		double[] suma = new double[coeficientes.length];
-		
+
 		// Vamos sumando los coeficientes de los dos polinomios
 		// y guardándolos en el array suma.
-		for (int i=0; i<coeficientes.length;i++){
+		for (int i = 0; i < coeficientes.length; i++) {
 			suma[i] = coeficientes[i] + coeficientesQ[i];
 		}
-		
-		
-		
+
 		return new Polinomio(suma);
 	}
-	
-	public void mostrarCoeficientes(){
-		// Muestra en una fila los coeficientes de este polinomio.
+
+	// Resta un polinomio q a nuestro polinomio.
+	// Damos por supuesto que ambos son del mismo grado.
+	public Polinomio restar(Polinomio q) {
 		
+		// Obtenemos los coeficientes de q
+		double[] coeficientesQ = q.getCoeficientes();
+
+		// Creamos un array de coeficientes que contendrá
+		// la resta de los coeficientes de los dos polinomios.
+		double[] resta = new double[coeficientes.length];
+
+		// Vamos restando los coeficientes de los dos polinomios
+		// y guardándolos en el array resta.
+		for (int i = 0; i < coeficientes.length; i++) {
+			resta[i] = coeficientes[i] - coeficientesQ[i];
+		}
+
+		return new Polinomio(resta);
+	}
+
+	public void mostrarCoeficientes() {
+		// Muestra en una fila los coeficientes de este polinomio.
+
 		System.out.print("Coeficientes: ");
-		for(int i = 0; i<coeficientes.length;i++){
+		for (int i = 0; i < coeficientes.length; i++) {
 			System.out.print(coeficientes[i] + " ");
 		}
+		System.out.println();
+	}
+	
+	public String toString(){
+		String salida = "";
+		double valor;
 		
+		for(int i=0;i<coeficientes.length;i++){
+			valor = coeficientes[i];
+			if(i>=2){
+				salida += "+" + String.valueOf(valor) + 
+						"x^" + i;
+			} else if(i>=1) {
+				salida += "+" + String.valueOf(valor) + 
+						"x";
+			} else{
+				salida += String.valueOf(valor);
+			}
+		}
+		
+		return salida;
 	}
 
 	public double[] getCoeficientes() {
@@ -65,19 +103,8 @@ public class Polinomio {
 	public void setCoeficientes(double[] coeficientes) {
 		this.coeficientes = coeficientes;
 	}
-	
-	
+
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
--- Tema5Proyecto7/src/inicio/Inicio.java
+++ Tema5Proyecto7/src/inicio/Inicio.java
@@ -8,14 +8,37 @@ public class Inicio {
 		
 		//double[] coeficientesP = {1,3,5};
 		//Polinomio p = new Polinomio(coeficientesP);
-		Polinomio p = new Polinomio(new double[]{1,3,5});
+		Polinomio p = new Polinomio(new double[]{1,3,5,10,3});
+		System.out.print("p = ");
+		p.mostrarCoeficientes();
 		
-		double[] coeficientesQ = {1,6,7};
+		double[] coeficientesQ = {1,6,7,4,5};
 		Polinomio q = new Polinomio(coeficientesQ);
+		System.out.print("q = ");
+		q.mostrarCoeficientes();
 		
 		Polinomio suma = p.sumar(q);
+		System.out.println("Suma de p y q = ");
+		suma.mostrarCoeficientes();
 		
+		Polinomio resta = p.restar(q);
+		System.out.println("Resta de p y q = ");
+		resta.mostrarCoeficientes();
 		
+		System.out.println("Llamamos al método toString de p:");
+		System.out.println(p.toString());
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
