765d1e29ee8406e4959d0792f533fa58003e145a
Carlos Sogorb <profesorderedes@gmail.com>
2017-02-22 19:00:41 +0100

    mié feb 22 19:00:41 CET 2017

--- Tema6Proyecto1/src/principal/Cliente.java
+++ Tema6Proyecto1/src/principal/Cliente.java
@@ -2,4 +2,8 @@ package principal;
 
 public class Cliente extends Persona{
 
+	public void mostrar(){
+		
+	}
+	
 }
--- Tema6Proyecto1/src/principal/Main.java
+++ Tema6Proyecto1/src/principal/Main.java
@@ -4,18 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Persona p = new Persona();
-		
-		
-		Cliente c = new Cliente();
-		c.setApellidos("Sánchez");
-		c.setNombre("Miguel");
-		c.setDni("23263123P");
-		
-		Empleado e = new Empleado();
-		
-		
-		
 	}
 
 }
